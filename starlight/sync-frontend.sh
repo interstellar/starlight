7594f45c95c4e393b9229cc5ccefe716086b54db
jessicard <jessicard@mac.com>
2018-10-17 15:31:36 -0700

    starlight: add script to sync frontend
    Since we're serving a react app from a go server, we want to streamline the
    process to
    ship any front-end updates. Instead of having to recompile the go server,
    do a PR, and do a
    full rebuild of the server binary, the process will be:
    
    1. Run a script, ` aws-vault exec i10r-users -- ./sync-frontend.sh`, that
    runs `npm run build` and uploads the compiled `public/` directory to S3
    1. The go server loads a single file at `index.html` that, with JS, fetches
    the webpack compiled `index.html` and replaces its contents
    
    
    
    Closes #5937
    
    Author: jessicard <jessicard@mac.com>
    Date: Wed Oct 17 15:31:36 2018 -0700
    upstream:1ac52853f842b7e5aa1a57ed21e20e267f082b1d

---
+++ starlight/sync-frontend.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# If you want to use your own S3 bucket, update 'starlight-client'
+# with your bucket name.
+cd wallet && npm run build && aws s3 sync public s3://starlight-client --acl public-read
