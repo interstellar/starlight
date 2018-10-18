6e261dda3a7d25deab09e14c3cb1e0d00af0983e
Bob Glickstein <bobg@users.noreply.github.com>
2018-10-18 08:55:46 -0700

    starlight/starlighttest: factor out common preamble from integration tests
    Closes #6009
    
    Author: Bob Glickstein <bobg@users.noreply.github.com>
    Date: Thu Oct 18 08:55:46 2018 -0700
    upstream:5ddc0db1bd00785519a6b5ced9baa218adfaa95b

--- starlight/starlighttest/integration_test.go
+++ starlight/starlighttest/integration_test.go
@@ -12,12 +12,13 @@ import (
 	"github.com/interstellar/starlight/starlight/xlm"
 )
 
-func TestChannelCreation(t *testing.T) {
+// itest runs an alice-and-bob integration test.
+func itest(t *testing.T, f func(ctx context.Context, alice, bob *Starlightd)) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
 	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "channel-creation")
+	testdir, err := ioutil.TempDir("", t.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,230 +27,115 @@ func TestChannelCreation(t *testing.T) {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+
 	alice := start(t, ctx, testdir, "alice")
 	defer alice.server.Close()
+
 	bob := start(t, ctx, testdir, "bob")
 	defer bob.server.Close()
 
-	steps := channelCreationSteps(alice, bob, 0, 0)
-	for _, s := range steps {
-		testStep(t, ctx, s, nil)
-	}
+	f(ctx, alice, bob)
+}
+
+func TestChannelCreation(t *testing.T) {
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := channelCreationSteps(alice, bob, 0, 0)
+		for _, s := range steps {
+			testStep(t, ctx, s, nil)
+		}
+	})
 }
 
 func TestChannelOpenCloseNoPayment(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "coop-close")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
-
-	steps := append(channelCreationSteps(alice, bob, 0, 0), aliceCoopCloseSteps(alice, bob, 0)...)
-	var channelID string
-	for _, step := range steps {
-		testStep(t, ctx, step, &channelID)
-	}
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 0, 0), aliceCoopCloseSteps(alice, bob, 0)...)
+		var channelID string
+		for _, step := range steps {
+			testStep(t, ctx, step, &channelID)
+		}
+	})
 }
 
 func TestChannelPayment(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "channel-pay")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
-
-	steps := append(channelCreationSteps(alice, bob, 0, 0), bobChannelPayAliceSteps(alice, bob)...)
-	var channelID string
-	for _, step := range steps {
-		testStep(t, ctx, step, &channelID)
-	}
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 0, 0), bobChannelPayAliceSteps(alice, bob)...)
+		var channelID string
+		for _, step := range steps {
+			testStep(t, ctx, step, &channelID)
+		}
+	})
 }
 
 func TestChannelSettleWithGuestCoopClose(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "settle-with-guest-coop-close")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
-
-	steps := append(channelCreationSteps(alice, bob, 0, 0), bobChannelPayAliceSteps(alice, bob)...)
-	steps = append(steps, aliceCoopCloseSteps(alice, bob, 1000*xlm.Stroop)...)
-	var channelID string
-	for _, step := range steps {
-		testStep(t, ctx, step, &channelID)
-	}
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 0, 0), bobChannelPayAliceSteps(alice, bob)...)
+		steps = append(steps, aliceCoopCloseSteps(alice, bob, 1000*xlm.Stroop)...)
+		var channelID string
+		for _, step := range steps {
+			testStep(t, ctx, step, &channelID)
+		}
+	})
 }
 
 func TestDuplicateChannelCreation(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "duplicate-channel")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
-
-	steps := append(channelCreationSteps(alice, bob, 0, 0), step{
-		name:  "bob create channel with alice",
-		agent: bob,
-		path:  "/api/do-create-channel",
-		body: fmt.Sprintf(`{
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 0, 0), step{
+			name:  "bob create channel with alice",
+			agent: bob,
+			path:  "/api/do-create-channel",
+			body: fmt.Sprintf(`{
 				"GuestAddr": "alice*%s", 
 				"HostAmount": 10000000000
 			}`, alice.address),
-		wantCode: http.StatusResetContent,
+			wantCode: http.StatusResetContent,
+		})
+		var channelID string
+		for _, step := range steps {
+			testStep(t, ctx, step, &channelID)
+		}
 	})
-	var channelID string
-	for _, step := range steps {
-		testStep(t, ctx, step, &channelID)
-	}
 }
 
 func TestChannelNoPaymentForceClose(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "no-payment-force-close")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
-
-	steps := append(channelCreationSteps(alice, bob, 1, 1), bobForceCloseStepsNoGuestBalance(alice, bob)...)
-	var channelID string
-	for _, step := range steps {
-		testStep(t, ctx, step, &channelID)
-	}
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 1, 1), bobForceCloseStepsNoGuestBalance(alice, bob)...)
+		var channelID string
+		for _, step := range steps {
+			testStep(t, ctx, step, &channelID)
+		}
+	})
 }
 
 func TestSettleWithGuestForceClose(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "settle-guest-force-close")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
-
-	steps := append(channelCreationSteps(alice, bob, 1, 1), bobChannelPayAliceSteps(alice, bob)...)
-	steps = append(steps, bobForceCloseStepsSettleWithGuest(alice, bob, 1000*xlm.Stroop)...)
-	var channelID string
-	for _, step := range steps {
-		testStep(t, ctx, step, &channelID)
-	}
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 1, 1), bobChannelPayAliceSteps(alice, bob)...)
+		steps = append(steps, bobForceCloseStepsSettleWithGuest(alice, bob, 1000*xlm.Stroop)...)
+		var channelID string
+		for _, step := range steps {
+			testStep(t, ctx, step, &channelID)
+		}
+	})
 }
 
 func TestHostTopUp(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "host-top-up")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 0, 0), hostTopUpSteps(alice, bob)...)
 
-	steps := append(channelCreationSteps(alice, bob, 0, 0), hostTopUpSteps(alice, bob)...)
-
-	var channelID string
-	for _, s := range steps {
-		testStep(t, ctx, s, &channelID)
-	}
+		var channelID string
+		for _, s := range steps {
+			testStep(t, ctx, s, &channelID)
+		}
+	})
 }
 
 func TestLogoutLogin(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	log.Printf("running %s", t.Name())
-	testdir, err := ioutil.TempDir("", "logout-login")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(testdir)
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	alice := start(t, ctx, testdir, "alice")
-	defer alice.server.Close()
-	bob := start(t, ctx, testdir, "bob")
-	defer bob.server.Close()
+	itest(t, func(ctx context.Context, alice, bob *Starlightd) {
+		steps := append(channelCreationSteps(alice, bob, 0, 0), logoutSteps(alice, bob)...)
+		steps = append(steps, loginSteps(alice, bob)...)
 
-	steps := append(channelCreationSteps(alice, bob, 0, 0), logoutSteps(alice, bob)...)
-	steps = append(steps, loginSteps(alice, bob)...)
-
-	var channelID string
-	for _, s := range steps {
-		testStep(t, ctx, s, &channelID)
-	}
+		var channelID string
+		for _, s := range steps {
+			testStep(t, ctx, s, &channelID)
+		}
+	})
 }
