907a3a63d5407912019f5afc93def76f85d675d3
jessicard <jessicard@mac.com>
2018-10-17 17:24:12 -0700

    starlight: clear client S3 when uploading new public files
    Add the `--delete` flag.
    --delete (boolean) Files that exist in the destination but not in the
    source are deleted during sync.
    
    https://docs.aws.amazon.com/cli/latest/reference/s3/sync.html
    
    Closes #5991
    
    Author: jessicard <jessicard@mac.com>
    Date: Wed Oct 17 17:24:12 2018 -0700
    upstream:f1a4a0b6ba49d5e185bc78aa8efe4051a0ad7ff5

--- starlight/sync-frontend.sh
+++ starlight/sync-frontend.sh
@@ -2,4 +2,4 @@
 
 # If you want to use your own S3 bucket, update 'starlight-client'
 # with your bucket name.
-cd wallet && npm run build && aws s3 sync public s3://starlight-client --acl public-read
+cd wallet && npm run build && aws s3 sync public s3://starlight-client --delete --acl public-read
